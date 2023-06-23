d3f7276d1d066aeb2136bdbc925d0df915d2dbbe
Fernando <fervasq@protonmail.com>
2023-06-23 12:19:00 -0400

    Remove the logic that plays 5 rounds. Make it only 1 round.

--- script.js
+++ script.js
@@ -5,10 +5,8 @@ function getComputerChoice() {
     return choice[randomIndex];
 }
 
-
 //function that plays a single round of rock paper scissors
 function playRound(playerSelection, computerSelection) {
-
     //Convert playerSelection to lowercase
     playerSelection = playerSelection.toLowerCase();
 
@@ -22,45 +20,15 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "scissors" && computerSelection === "paper")
-    )   {
+    ) {
         return "You win! " + playerSelection + " beats " + computerSelection;
-    }   else {
+    } else {
         return "You lose! " + computerSelection + " beats " + playerSelection;
     }
 }
 
-
-//Play a 5 round game that keeps score and reports a winner or loser at the end
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
-    // Repeat the function call 5 times using a for loop
-    for (let round = 1; round <= 5; round ++) {
-        const playerSelection = prompt("Enter your choice (rock, paper or scissors):");
-        const computerSelection = getComputerChoice();
-        const result = playRound(playerSelection, computerSelection);
-
-        console.log(`Round ${round}: ${result}`);
-
-        if (result.startsWith("You win!")) {
-            playerScore++;
-        }   else if (result.startsWith("You lose!")) {
-            computerScore++;
-        }
-    }
-
-    console.log(`Final Score: Player ${playerScore} - ${computerScore} Computer`);
-
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You win the game");
-    }   else if (playerScore < computerScore) {
-        console.log("Oops! You lose the game!");
-    }   else {
-        console.log("It's a tie! The game ends in a draw.");
-    }
-}
-
-
 //Start the game
-game();
+const playerSelection = prompt("Enter your choice (rock, paper, or scissors):");
+const computerSelection = getComputerChoice();
+const result = playRound(playerSelection, computerSelection);
+console.log(result);
