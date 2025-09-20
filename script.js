ba4001be45795c17879593bfba4c2a9dd69d51ca
Fernando <59489587+fercodes@users.noreply.github.com>
2025-09-19 21:19:57 -0400

    Declare the players score variables, write the logic to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getHumanChoice() {
+  let humanChoice = prompt('Enter rock, paper, or scissors:').toLowerCase();
+  return humanChoice;
+}
+
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3);
   if (computerChoice === 0) {
@@ -9,10 +17,28 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  let humanChoice = prompt('Enter rock, paper, or scissors:').toLowerCase();
-  return humanChoice;
+function playRound(humanChoice, computerChoice) {
+  console.log('You chose: ' + humanChoice);
+  console.log('Computer chose: ' + computerChoice);
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie! You both chose " + humanChoice);
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    console.log('You win! ' + humanChoice + ' beats ' + computerChoice);
+    humanScore++;
+  } else {
+    console.log('You lose! ' + computerChoice + ' beats ' + humanChoice);
+    computerScore++;
+  }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log('Human: ' + humanScore + ' | Computer: ' + computerScore);
